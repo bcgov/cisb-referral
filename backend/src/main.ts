2afed027dbc7a0f168398d33513438e776de3660
Adam Hodgins <75464641+hodginsadam@users.noreply.github.com>
2026-02-04 14:32:54 -0800

    Use bootstrap().then/catch and import type

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,17 +1,15 @@
+import type { NestExpressApplication } from '@nestjs/platform-express';
 import { bootstrap } from './app';
 import { Logger } from '@nestjs/common';
 
 const logger = new Logger('NestApplication');
 
-async function main() {
-  try {
-    const app = await bootstrap();
+bootstrap()
+  .then(async (app: NestExpressApplication) => {
     await app.listen(3000);
     logger.log(`Listening on ${await app.getUrl()}`);
     logger.log(`Process start up took ${process.uptime()} seconds`);
-  } catch (err) {
+  })
+  .catch((err) => {
     logger.error(err);
-  }
-}
-
-main();
+  });
